95cc579372be48806c1af203b14321a5e2a9c930
Seth Lee <sethlee@seths-air.attlocal.net>
2021-07-14 16:25:42 -0400

    get request updated

--- src/components/LogMeal.jsx
+++ src/components/LogMeal.jsx
@@ -5,11 +5,6 @@ import { BASE_URL, headers } from '../services';
 import { useState } from 'react';
 
 
-// const AIRTABLE_KEY = process.env.REACT_APP_AIRTABLE_KEY;
-// const AIRTABLE_BASE = process.env.REACT_APP_AIRTABLE_BASE;
-// const BASE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/Table%201`;
-// const headers = { Authorization: `Bearer ${AIRTABLE_KEY}` };
-
 
   const defaultForm = {
     date: '',
--- src/components/MealHistory.jsx
+++ src/components/MealHistory.jsx
@@ -1,27 +1,25 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-// import { BASE_URL, headers } from "../services";
+import { BASE_URL, headers } from "../services";
 
-const AIRTABLE_KEY = process.env.REACT_APP_AIRTABLE_KEY;
-const AIRTABLE_BASE = process.env.REACT_APP_AIRTABLE_BASE;
-const BASE_URL = `https://api.airtable.com/v0/${AIRTABLE_BASE}/Table%201`;
-const headers = { Authorization: `Bearer ${AIRTABLE_KEY}` };
+
 
 export default function MealHistory() {
-  const [meals, SetMeals] = useState([]);
+  const [meals, setMeals] = useState([]);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const res = await axios.get(BASE_URL, { headers });
-      console.log(res.data.records)
-      // setMeals(res.data.records)
+      setMeals(res.data.records)
     };
     fetchMeals();
   }, []);
   
   return (
     <div>
-    list
+      {meals.map((meal) => {
+        return <div>{meal.fields.food}</div>;
+    })}
     </div>
   )
 }
