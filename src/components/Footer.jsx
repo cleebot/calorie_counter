5d87f74bf6543aaf8b6792af5bfe13a2607e0074
Seth Lee <sethlee@seths-air.attlocal.net>
2021-07-16 12:21:47 -0400

    footer edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "./Footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <div className="footer">
-      <p>Created by Caleb Lee</p>
+      Caleb Lee
+      <a href="https://github.com/cleebot"><FontAwesomeIcon icon={faGithub} className="git" target="_blank" /></a>
+      <a href="https://linkedin.com/in/caleblee92"><FontAwesomeIcon icon={faLinkedin} className="git" target="_blank"/></a>
     </div>
   )
 }
