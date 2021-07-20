294283243c5f165ce42e9bd50c33b35d88a0c2a1
Seth Lee <sethlee@seths-air.attlocal.net>
2021-07-20 13:55:17 -0400

    spelling errors

--- src/components/DietInfo.jsx
+++ src/components/DietInfo.jsx
@@ -7,7 +7,7 @@ export default function DietInfo() {
       <h3>Many diet plans require you to keep track of certain nutritional values in order to make sure that you can achieve the goals that you set for yourself.
         Listed below are some details and resources for popular diet plans that people follow based on what they want to achieve!</h3>
       <p><b>Atkins Diet</b>: <br />
-        The Atkins diet's main idea is to eat lots of protein while consuming very little carbohydrates. Studies have shown that the Atkins diet is goood for weight loss and improvemnets in blood sugar and triglycerides.
+        The Atkins diet's main idea is to eat lots of protein while consuming very little carbohydrates. Studies have shown that the Atkins diet is good for weight loss and improvemnets in blood sugar and triglycerides.
         For a more detailed look into the diet plan, please check out the resources below.<br />
         <a href="https://www.healthline.com/nutrition/atkins-diet-101#foods-to-avoid/" target="_blank" rel="noreferrer">Atkin's Diet</a></p><br />
       <p><b>Intermittent Fasting</b>:<br />
